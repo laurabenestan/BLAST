3d45652169ca11021020c8e8483e98b1824650f4
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2020-04-16 09:02:14 +0200

    txt to tsv

--- call_exon_sequences.sh
+++ call_exon_sequences.sh
@@ -14,17 +14,23 @@ singularity shell --bind /media/superdisk:/media/superdisk $SINGULARITY_SIMG
 ## global variables
 ### diplodus
 GENOME_FASTA="/media/superdisk/reservebenefit/donnees/genomes/sar_genome_lgt6000.fasta"
-OUTLIERS="data/476outliers_pcadapt_diplodus.tsv"
+#OUTLIERS="476outliers_pcadapt_diplodus.tsv"
+#OUTLIERS="193rdaoutliers_diplodus.txt"
+OUTLIERS="47588allsnps_diplodus.txt"
 SPECIES="diplodus"
 GFF3="/media/superdisk/reservebenefit/working/annotation/DSARv1_annotation.gff3"
 ### mullus
 GENOME_FASTA="/media/superdisk/reservebenefit/donnees/genomes/mullus_genome_lgt6000.fasta"
-OUTLIERS="data/367outliers_pcadapt_mullus.tsv"
+OUTLIERS="367outliers_pcadapt_mullus.tsv"
+OUTLIERS="199rdaoutliers_mullus.txt"
+OUTLIERS="36613allsnps_mullus.txt"
 SPECIES="mullus"
 GFF3="/media/superdisk/reservebenefit/working/annotation/MSURv1_annotation.gff3"
 ### serran
 GENOME_FASTA="/media/superdisk/reservebenefit/donnees/genomes/serran_genome_lgt3000.fasta"
-OUTLIERS="data/641outliers_pcadapt_serranus.tsv"
+OUTLIERS="641outliers_pcadapt_serranus.tsv"
+OUTLIERS="311rdaoutliers_serranus.txt"
+OUTLIERS="64057allsnps_serranus.txt"
 SPECIES="serran"
 GFF3="/media/superdisk/reservebenefit/working/annotation/SCABv1_annotation.gff3"
 
@@ -33,8 +39,11 @@ GFF3="/media/superdisk/reservebenefit/working/annotation/SCABv1_annotation.gff3"
 EXOME="exome/"$SPECIES"_exon.gff3"
 awk '{ if($3 =="exon") { print $0 } }' $GFF3 > $EXOME
 ## convert into bed
-awk '{ print $1"\t"$2"\t"$2+1 }' $OUTLIERS > processing/"$SPECIES"_selected_loci.bed
+OUTBED=processing/"${OUTLIERS/.tsv/_selected_loci.bed}"
+awk '{ print $1"\t"$2"\t"$2+1 }' data/$OUTLIERS > $OUTBED
 ## get coding region for SNPs
-bedtools intersect -wb -a processing/"$SPECIES"_selected_loci.bed -b "$EXOME" > processing/"$SPECIES"_coding.snps.bed
+CODINGBED=processing/"${OUTLIERS/.tsv/_coding.snps.bed}"
+bedtools intersect -wb -a $OUTBED -b $EXOME > $CODINGBED
 ## format annotation table (get genome sequences with 99*2 flanking region)
-python3 flanking_sequence.py -g "$GENOME_FASTA" -t processing/"$SPECIES"_coding.snps.bed -f 99 > sequences/"$SPECIES".snps.csv
+SNPSCSV=sequences/"${OUTLIERS/.tsv/.snps.csv}"
+python3 flanking_sequence.py -g $GENOME_FASTA -t $CODINGBED -f 99 > $SNPSCSV
