c9f7458e0848c3d24abba80cd1bb874d3d76304a
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2020-04-16 09:02:59 +0200

    rename txt into tsv

--- call_exon_sequences.sh
+++ call_exon_sequences.sh
@@ -15,22 +15,22 @@ singularity shell --bind /media/superdisk:/media/superdisk $SINGULARITY_SIMG
 ### diplodus
 GENOME_FASTA="/media/superdisk/reservebenefit/donnees/genomes/sar_genome_lgt6000.fasta"
 #OUTLIERS="476outliers_pcadapt_diplodus.tsv"
-#OUTLIERS="193rdaoutliers_diplodus.txt"
-OUTLIERS="47588allsnps_diplodus.txt"
+#OUTLIERS="193rdaoutliers_diplodus.tsv"
+OUTLIERS="47588allsnps_diplodus.tsv"
 SPECIES="diplodus"
 GFF3="/media/superdisk/reservebenefit/working/annotation/DSARv1_annotation.gff3"
 ### mullus
 GENOME_FASTA="/media/superdisk/reservebenefit/donnees/genomes/mullus_genome_lgt6000.fasta"
 OUTLIERS="367outliers_pcadapt_mullus.tsv"
-OUTLIERS="199rdaoutliers_mullus.txt"
-OUTLIERS="36613allsnps_mullus.txt"
+OUTLIERS="199rdaoutliers_mullus.tsv"
+OUTLIERS="36613allsnps_mullus.tsv"
 SPECIES="mullus"
 GFF3="/media/superdisk/reservebenefit/working/annotation/MSURv1_annotation.gff3"
 ### serran
 GENOME_FASTA="/media/superdisk/reservebenefit/donnees/genomes/serran_genome_lgt3000.fasta"
 OUTLIERS="641outliers_pcadapt_serranus.tsv"
-OUTLIERS="311rdaoutliers_serranus.txt"
-OUTLIERS="64057allsnps_serranus.txt"
+OUTLIERS="311rdaoutliers_serranus.tsv"
+OUTLIERS="64057allsnps_serranus.tsv"
 SPECIES="serran"
 GFF3="/media/superdisk/reservebenefit/working/annotation/SCABv1_annotation.gff3"
 
