96113ea982883b8b1d9b73e7a3bf0a7f349b38ce
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2020-04-17 17:58:57 +0200

    init download transcriptome sra file

---
+++ get_transcriptome.sh
@@ -0,0 +1,7 @@
+## download Muscle of Spiny lobster transcriptome
+## https://www.ncbi.nlm.nih.gov/sra/SRX6760688[accn]
+
+wget -O transcriptomes/muscle_lobster.sra  https://sra-download.ncbi.nlm.nih.gov/traces/sra9/SRR/009788/SRR10023610
+
+## sra toolkit
+
