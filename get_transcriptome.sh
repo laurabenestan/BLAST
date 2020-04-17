bb0cedee93a051cff9d9bb6102753be05bac3ff6
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2020-04-17 19:36:34 +0200

    sra to fastq

--- get_transcriptome.sh
+++ get_transcriptome.sh
@@ -3,5 +3,7 @@
 
 wget -O transcriptomes/muscle_lobster.sra  https://sra-download.ncbi.nlm.nih.gov/traces/sra9/SRR/009788/SRR10023610
 
-## sra toolkit
+## load an environment with sra toolkit
+SINIMG="singularity exec --bind /media/superdisk:/media/superdisk snpsdata_analysis.simg"
 
+nohup $SINIMG fastq-dump -I --split-files transcriptomes/muscle_lobster.sra &
