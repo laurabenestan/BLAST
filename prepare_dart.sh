eb129e35c4a7137d6fd62f1114ac6e3da773b319
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2020-04-17 17:58:41 +0200

    command to set dart vcf to fas

--- prepare_dart.sh
+++ prepare_dart.sh
@@ -3,3 +3,4 @@
 DART_RAW="/media/superdisk/reservebenefit/donnees/dart/palinurus/infos/Report_DSpl19-4025_SNP_2.csv"
 grep -v "*" $DART_RAW  | cut -d "," -f 2,3 | sort | uniq | awk '{ printf ">%s\n%s\n",$1,$2 }' | tr ',' '\n' | sed '/^$/d' > data/palinurus_variants_dart.fasta
 
+
